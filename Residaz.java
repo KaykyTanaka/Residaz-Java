72374df2fc065892b681d9be1760c7e396293123
Kayky <tanaka.kayky@gmail.com>
2023-06-17 20:28:14 -0300

    Classe main

---
+++ Residaz.java
@@ -0,0 +1,16 @@
+/* Guilherme Afonso - Kayky Tanaka - Rafael Augusto - Rhômulo Mourão
+ * 
+ * Residaz.java
+ * 
+ * 
+ * 
+ */
+
+
+public class Residaz{
+	
+	public static void main (String[] args) {
+		
+	}
+}
+
