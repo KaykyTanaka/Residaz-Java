30d4cbbd151edd562989d04d4e9507e05171fc3c
Rafaelbadas <59000602+Rafaelbadas@users.noreply.github.com>
2023-06-20 11:13:40 -0300

    Add files via upload

---
+++ PIProjeto/Residaz.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Residaz {
+	public static void main (String[] args) {
+		Scanner ler = new Scanner(System.in);
+		
+		
+	}
+}
+
---
+++ PIProjeto/Zelador.java
@@ -0,0 +1,8 @@
+public class Zelador extends Pessoa {
+	private Manutencao manutencao;
+	
+	public Zelador( String nome, String cpf, String telefone, String email, String tipo  ){
+		super( nome, cpf, telefone, email, tipo);
+	}
+}
+
