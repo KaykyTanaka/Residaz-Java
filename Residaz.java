0ab26c5b74ecb4dcfd28c73cd3b82c3f776ad39b
Kayky Tanaka <111456230+KaykyTanaka@users.noreply.github.com>
2023-06-21 18:41:13 -0300

    Update Residaz.java

--- Residaz.java
+++ Residaz.java
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-import java.util.Scanner;
-public class Residaz {
-	public static void main (String[] args) {
-		Scanner ler = new Scanner(System.in);
-		
-		
-	}
-}
-
-=======
 import java.util.Scanner;
 public class Residaz {
 	public static void main (String[] args) {
